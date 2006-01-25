73e68adacce7ca97962687ef58018bd210973a24
Fabian Kloosterman <fkloos@mit.edu>
2006-01-25 00:02:40 +0000

    add subsref fixedrecordfile

---
+++ @mwlfixedrecordfile/subsref.m
@@ -0,0 +1,20 @@
+function b = subsref(frf,s)
+%SUBSREF subscripted indexing
+%
+%  Syntax
+%
+%      f.property
+%
+
+%  Copyright 2005-2006 Fabian Kloosterman
+
+switch s.type
+case '.'
+    if strcmp(s.subs, 'nrecords')
+        b = get(frf, 'nrecords');
+    elseif strcmp(s.subs, 'recordsize')
+        b = rfb.recordsize;
+    else
+        b = subsref(frf.mwlrecordfilebase, s);
+    end
+end
