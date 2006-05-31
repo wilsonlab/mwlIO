714a0ea32463dc39246718559141a918785f3d07
Fabian Kloosterman <fkloos@mit.edu>
2006-05-31 23:57:42 +0000

    dunno

---
+++ @mwlfield/bytesize.m
@@ -0,0 +1,17 @@
+function retval = bytesize( field )
+%SIZE return the total field size in bytes
+%
+%  Syntax
+%
+%      sz = size( field )
+%
+%  Examples
+%
+%      field = mwlfield( 'test', 'short', 2 );
+%      size( field )  % will return: 6
+%
+
+%  Copyright 2006-2006 Fabian Kloosterman
+
+retval = mwltypemapping( [field.type], 'code2size');
+retval = retval .* length( field );
