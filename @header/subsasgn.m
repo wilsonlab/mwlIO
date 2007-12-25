3c43737410a85e39f26a6c6ac13780c02eb10450
Fabian Kloosterman <fkloos@mit.edu>
2007-12-25 11:41:46 +0000

    updated help

--- @header/subsasgn.m
+++ @header/subsasgn.m
@@ -2,14 +2,15 @@ function h = subsasgn(h,s,b)
 %SUBSASGN subscripted assignment
 %
 %  h=SUBSASGN(h, subs, val) subscripted assignment to add (or replace)
-%  subheaders to a header object.
+%  subheaders to a header object and to set parameters in subheaders.
 %
 %  Example
-%    sh = subheader();
-%    sh = setParam( sh, 'Test', 1)
-%    h = header();
-%    h(1) = sh;
-%
+%    sh = newheader('a',1,'b',2);
+%    h = header(sh);
+%    h(2:3) = sh; %create 2nd and 3rd subheader
+%    h(2).a = 7; %set parameter in 2nd subheader
+%    %parameter assignment to multiple subheaders is NOT allowed:
+%    h(2:3).b = 9;
 
 %  Copyright 2005-2006 Fabian Kloosterman
 
--- @header/subsref.m
+++ @header/subsref.m
@@ -2,22 +2,23 @@ function b = subsref(h,s)
 %SUBSREF subscripted indexing
 %
 %  val=SUBSREF(h, subs) allow subscripted indexing to retrieve subheaders
-%  from a header.
+%  and parameters from a header.
 %
 %  Example
-%    hdr = header();
-%    sh = subheader();
-%    sh = setParam(sh, 'test', 1);
-%    hdr(1) = sh;
-%    sh = hdr(1);
+%    sh = newheader('parm1', 1, 'parameter two', 2);
+%    hdr = header( sh );
+%    hdr(1) %retrieves first subheader
+%    p = hdr(1).parm1; %retrieves parameter from subheader 1
+%    p = hdr(:).('parameter two'); %retrieves parameter from all subheaders
 %
 
 %  Copyright 2005-2006 Fabian Kloosterman
 
+
 switch s(1).type
 case '()'
  ind = s(1).subs{:};
- b = h.subheaders(ind);
+ b = header(h.subheaders(ind));
  if numel(s)>1
    switch s(2).type
     case '.'
