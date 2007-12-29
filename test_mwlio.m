279f9ac0be09032dd5e6942806c5f13e5c700787
Fabian Kloosterman <fkloos@mit.edu>
2007-12-29 04:15:33 +0000

    removed dependency on fkCircular toolbox

--- test_mwlio.m
+++ test_mwlio.m
@@ -4,15 +4,15 @@ function test_mwlio()
 %  TEST_MWLIO creates a temporary fixed record file and writes artificial
 %  data to it. Then reopens the file and reads the data and uses this
 %  data to write it again to disk. This test use the following functions:
-%  header, newheader, mwlfield, mwlcreate, mwlopen, load, loadfield.
+%  header, mwlfield, mwlcreate, mwlopen, load, loadfield.
 %
 
-%  Copyright 2005-2006 Fabian Kloosterman
+%  Copyright 2005-2008 Fabian Kloosterman
 
 filename = tempname;
 
 %create new header
-hdr = header( newheader( 'TestDate', datestr(now) ) );
+hdr = header( 'TestDate', datestr(now) );
 
 %create data
 data.scalar = 1:100;
@@ -20,9 +20,9 @@ data.vector = [1 2 3]' * (1:100);
 fcn = @(x,y, xc, yc, alpha, scale) cos( ( (x-xc).*cos(pi/3+alpha) + (y-yc).*sin(pi/3+alpha) )./scale ) + cos( ( (x-xc).*cos(alpha) + (y-yc).*sin(alpha) )./scale ) + cos( ( (x-xc).*cos(-pi/3+alpha) + (y-yc).*sin(-pi/3+alpha) )./scale );
 x = repmat( linspace(-4*pi, 4*pi, 20 ), 20, 1 );
 
-offset_x = 2.*circ_unifrnd( 100,1 );
-offset_y = 2.*circ_unifrnd( 100,1 );
-rotation = circ_unifrnd( 100,1 );
+offset_x = 4*pi*rand(100,1 );
+offset_y = 4*pi*rand(100,1 );
+rotation = 2*pi*rand(100,1 );
 
 for k=1:100
     data.matrix(1:20,1:20,k) = fcn(x', x, offset_x(k), offset_y(k), rotation(k),1 );
